796fd5e3726784080cbec41d936426e2851e0bef
Nidre <unique.ek@gmail.com>
2016-08-12 18:17:41 +0300

    missin asset added.

---
+++ Assets/Scripts/Extensions/PathExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+public static class PathExtensions
+{
+    /// <summary>
+    /// Combines paths with forward slashes to be used in Unity Resources Load.
+    /// </summary>
+    /// <param name="path1"></param>
+    /// <param name="path2"></param>
+    /// <returns></returns>
+    public static string CombineForward(string path1, string path2)
+    {
+        return Path.Combine(path1, path2).Replace("\\", "/");
+    }
+}
